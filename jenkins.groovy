6dfd3f4eb52675e7d2ef8ef293b05c23328a4bb6
jainmnsh <jainmnsh@yahoo.com>
2020-03-13 19:01:45 -0700

    adding retag json

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 node ('master') {
-    //Clone jfro project from GitHub repository
+    //Clone jfrog report project from GitHub repository
     git url: 'https://github.com/jainmnsh/jfrog-report.git', branch: 'master'
     def rtServer = Artifactory.server SERVER_ID
     def buildDepInfo = Artifactory.newBuildInfo()
@@ -17,6 +17,7 @@ node ('master') {
     stage ('Build & Deploy') {
         dir ('./') {
             tagDockerApp = "${ARTDOCKER_REGISTRY}/artifact-report:${env.BUILD_NUMBER}"
+            tagDockerLatest =  "${ARTDOCKER_REGISTRY}/artifact-report:latest"
             docker.build(tagDockerApp)
             withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: CREDENTIALS, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
                 rtDocker = Artifactory.docker server: rtServer
@@ -28,11 +29,12 @@ node ('master') {
      }
     //Test docker image
      stage ('Test') {
-        dir('automation/artifact-report/app-test') {
+        dir('app/app-test') {
             sh 'docker rmi '+tagDockerApp+' || true'
             rtDocker.pull (tagDockerApp)
             if (testApp(tagDockerApp)) {
                   println "Setting property and promotion"
+                  //rtDocker.tag(tagDockerApp, tagDockerLatest)
                   sh 'docker rmi '+tagDockerApp+' || true'
              } else {
                   currentBuild.result = 'UNSTABLE'
@@ -59,26 +61,27 @@ node ('master') {
 
     //Promote docker image from staging local repo to production repo in Artifactory
      stage ('Promote') {
-        dir('automation/artifact-report/app-test') {
+        dir('app/app-test') {
             def promotionConfig = [
               'buildName'          : env.JOB_NAME,
               'buildNumber'        : env.BUILD_NUMBER,
               'targetRepo'         : PROMOTE_REPO,
               'comment'            : 'App works with latest released version of most used artifacts',
-              'sourceRepo'         : 'docker-stage-local',
+              'sourceRepo'         : SOURCE_REPO,
               'status'             : 'Released',
               'includeDependencies': false,
               'copy'               : true
             ]
             rtServer.promote promotionConfig
-            reTagLatest ()
+            reTagLatest (SOURCE_REPO)
+            reTagLatest (PROMOTE_REPO)
         }
      }
 }
 
 
 def testApp (tag) {
-    docker.image(tag).withRun('-p 9191:8181') {c ->
+    docker.image(tag).withRun('-p 9191:8080') {c ->
         sleep 10
         def stdout = sh(script: 'curl "http://localhost:9191/reports/artifacts/2"', returnStdout: true)
         if (stdout.contains("downloadCount")) {
@@ -93,16 +96,22 @@ def testApp (tag) {
 
 
 //Tag docker image
-def reTagLatest () {
-
-    def BUILD_NUMBER = env.BUILD_NUMBER
-    sh 'sed -E "s/@/$BUILD_NUMBER/" retag.json > retag_out.json'
-    sh 'sed -E "s/TARGETREPO/${PROMOTE_REPO}/" retag_out.json > retaga_out.json'
-    sh 'cat retaga_out.json'
-    withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: CREDENTIALS, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-        def curlString = "curl -u " + env.USERNAME + ":" + env.PASSWORD + " " + SERVER_URL
-        def regTagStr = curlString +  "/api/docker/${PROMOTE_REPO}/v2/promote -X POST -H 'Content-Type: application/json' -T retaga_out.json"
-        println "Curl String is " + regTagStr
-        sh regTagStr
-    }
+def reTagLatest (targetRepo) {
+     def BUILD_NUMBER = env.BUILD_NUMBER
+     sh 'sed -E "s/@/$BUILD_NUMBER/" retag.json > retag_out.json'
+     switch (targetRepo) {
+          case PROMOTE_REPO :
+              sh 'sed -E "s/TARGETREPO/${PROMOTE_REPO}/" retag_out.json > retaga_out.json'
+              break
+          case SOURCE_REPO :
+               sh 'sed -E "s/TARGETREPO/${SOURCE_REPO}/" retag_out.json > retaga_out.json'
+               break
+      }
+      sh 'cat retaga_out.json'
+      withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: CREDENTIALS, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+          def curlString = "curl -u " + env.USERNAME + ":" + env.PASSWORD + " " + SERVER_URL
+          def regTagStr = curlString +  "/api/docker/$targetRepo/v2/promote -X POST -H 'Content-Type: application/json' -T retaga_out.json"
+          println "Curl String is " + regTagStr
+          sh regTagStr
+      }
 }
